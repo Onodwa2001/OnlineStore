e93626ba54f280edc37342ba44713b68dd95a21d
KaraboMagagula <charitykarabomagagula@gmail.com>
2023-03-12 22:40:27 +0200

    added Customer class

---
+++ src/test/java/za/ac/cput/onlinestore/CustomerTest.java
@@ -0,0 +1,32 @@
+package za.ac.cput.onlinestore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+class CustomerTest {
+    @Test
+    public void objectEquality(){
+        Customer customer = new Customer("K09752", "Karabo", "Magagula", "magagula@gmail.com");
+        assertNotNull(customer);
+    }
+
+    @Test
+    public void objectIdentity(){
+        Customer customer = new Customer("09752", "Karabo", "Magagula", "magagula@gmail.com");
+        assertSame(customer, customer);
+    }
+
+    @Test
+    public void failingTest(){
+        Customer customer = new Customer("09752", "Karabo", "Magagula", "magagula@gmail.com");
+        System.out.println(customer.toString());
+        assertNotNull(customer);
+    }
+
+
+
+
+}
+
